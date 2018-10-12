33dd5bb40fa097fced73809be69ded7bc16ba8e2
Test <test@MacBook-Pro.local>
2018-10-12 04:20:47 +0300

    serv

--- main.py
+++ main.py
@@ -1,32 +1,62 @@
 import pyaudio
-import keyboard
+from http.server import BaseHTTPRequestHandler, HTTPServer
+import os
 
-CHUNK = 1024
-WIDTH = 2
-CHANNELS = 2
-RATE = 44100
-RECORD_SECONDS = 10
 
-p = pyaudio.PyAudio()
+class SHMATHTTPRequestHandler(BaseHTTPRequestHandler):
+    def do_GET(self):
+        # send code 200 response
+        self.send_response(200)
 
-stream = p.open(format=p.get_format_from_width(WIDTH),
-                channels=CHANNELS,
-                rate=RATE,
-                input=True,
-                output=True,
-                frames_per_buffer=CHUNK)
+        self.send_header("Connection", "Keep-Alive")
+        self.send_header("Access-Control-Allow-Origin", "*")
+        self.send_header("X-Content-Type-Options", "nosniff")
+        self.send_header("Transfer-Encoding", "chunked")
+        self.send_header("Content-Type", "audio/wave")
+        self.end_headers()
 
-print("* recording")
+        CHUNK = 512
 
-##for i in range(0, int(RATE / CHUNK * RECORD_SECONDS)):
+        WIDTH = 2
+        CHANNELS = 1
+        RATE = 44100
+        RECORD_SECONDS = 10
 
-while True:
-    data = stream.read(CHUNK)  # read audio stream
-    stream.write(data, CHUNK)  # play back audio stream
+        p = pyaudio.PyAudio()
 
-print("* done")
+        stream = p.open(format=pyaudio.paInt16,  # p.get_format_from_width(WIDTH),
+                        channels=CHANNELS,
+                        rate=RATE,
+                        input=True,
+                        output=True,
+                        frames_per_buffer=CHUNK)
 
-stream.stop_stream()
-stream.close()
+        ##for i in range(0, int(RATE / CHUNK * RECORD_SECONDS)):
+        while True:
+            data = stream.read(CHUNK)  # read audio stream
+            # stream.write(data, CHUNK)  # play back audio stream
 
-p.terminate()
+            # send file content to client
+
+            self.wfile.write(data)
+            self.wfile.flush()
+            if not data:
+                break
+
+
+def run():
+    try:
+        print('http server is starting...')
+        # ip and port of servr
+        # by default http server port is 80
+        server_address = ('127.0.0.1', 8005)
+        httpd = HTTPServer(server_address, SHMATHTTPRequestHandler)
+        print('http server is running...')
+        httpd.serve_forever()
+    except KeyboardInterrupt:
+        print('^C received, shutting down server')
+        httpd.socket.close()
+
+
+if __name__ == '__main__':
+    run()
